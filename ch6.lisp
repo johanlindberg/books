046524351269c69d40df9b7ae325c35f267b5914
Johan Lindberg <johan@pulp.se>
2008-11-28 21:37:32 +0100

    Took away some debugging defuns and implemented 'else'

--- ch6.lisp
+++ ch6.lisp
@@ -8,16 +8,10 @@
       (min 0)
       (max 0))
 
-  (defun head-pos () head)
-
   (defun make-turing-machine (&key starting-state)
     (let ((next-configuration starting-state)
 	  (step 0))
       (clear-tape)
-      (defun fake-step (arg)
-	(setf next-configuration arg)
-	(incf step)
-	(print-tape :step step :next-conf next-configuration))
       (defun next (&optional (n 1))
 	(dotimes (i n) 
 	  (setf next-configuration (funcall next-configuration))
@@ -67,16 +61,20 @@
 (defmacro m-configuration (name &body states)
   `(defun ,name ()
      (cond ,@(mapcar #'(lambda (state)
-			 `((eql (read-symbol) ',(car state))
-			   ,@(cadr state)
-			   ',(caddr state)))
+			 (cond ((eql (car state) 'else)
+				`(t ,@(cadr state)
+				    ',(caddr state)))
+			       (t `((eql (read-symbol) ',(car state))
+				    ,@(cadr state)
+				    ',(caddr state)))))
 		     states))))
 
 ;;; Here are the example Turing machine configurations from the book. I've
 ;;; prefixed states with "." in order to avoid name clashes with tape operations.
 ;;; In the book, and in Turing's paper, this is done by using different fonts.
 
-;; Computes all positive integers in sequence 0, 1, 10, 11, 100, ..
+;; Computes all positive integers in sequence
+;; This machine does it in binary (0, 1, 10, 11, 100, ..)
 (defmacro p99 ()
   `(progn
      (m-configuration begin
@@ -87,8 +85,30 @@
        (nil ((P 1)) rewind))
      (m-configuration rewind
        (nil ((L)) increment)
-       (0 ((R)) rewind)
-       (1 ((R)) rewind))
+       (else ((R)) rewind))
+
+     (make-turing-machine :starting-state 'begin)))
+
+;; and this machine in decimal (1, 2, 3, 4, 5, ..)
+(defmacro p100 ()
+  `(progn
+     (m-configuration begin
+       (nil ((P 0)) increment))
+     (m-configuration increment
+       (0 ((P 1)) rewind)
+       (1 ((P 2)) rewind)
+       (2 ((P 3)) rewind)
+       (3 ((P 4)) rewind)
+       (4 ((P 5)) rewind)
+       (5 ((P 6)) rewind)
+       (6 ((P 7)) rewind)
+       (7 ((P 8)) rewind)
+       (8 ((P 9)) rewind)
+       (9 ((P 0) (L)) increment)
+       (nil ((P 1)) rewind))
+     (m-configuration rewind
+       (nil ((L)) increment)
+       (else ((R)) rewind))
 
      (make-turing-machine :starting-state 'begin)))
 
